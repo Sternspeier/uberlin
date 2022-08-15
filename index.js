ab0869bec61c049789f96f5c559b2c44dea7a16c
Sternspeier <anna.winkler95@web.de>
2022-08-15 20:27:50 +0200

    more text

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-import "jquery";
+import "./jquery";
 
 function trueDate(){
-    const dateElement = document.getElementById("trueDate");
-    const d = new Date();
-    dateElement.innerHTML = String(d.getFullYear() + 1);
+    window.addEventListener("load", function(){
+        const dateElement = document.getElementById("trueDate");
+        const d = new Date();
+        dateElement.innerHTML = String(d.getFullYear() + 1);
+    });
 };
