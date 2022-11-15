0dd89cdf3ecbebfc030d89e21ed1caa498a00734
Sternspeier <anna.winkler95@web.de>
2022-11-15 21:44:11 +0100

    routing start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TextloaderComponent } from './components/textloader/textloader.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { TextWrapperComponent } from './components/text-wrapper/text-wrapper.component';
+import { DefaultPageComponent } from './components/default-page/default-page.component';
+import { AboutComponent } from './components/about/about.component';
+
+const appRoutes: Routes = [
+  {path: '', component: DefaultPageComponent},
+  {path: 'about', component: AboutComponent}
+]
 
 @NgModule({
   declarations: [
@@ -13,10 +21,13 @@ import { TextWrapperComponent } from './components/text-wrapper/text-wrapper.com
     TextloaderComponent,
     HeaderComponent,
     SidebarComponent,
-    TextWrapperComponent
+    TextWrapperComponent,
+    DefaultPageComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
